ae3708acddcde617d82338dbe0c625e612606e90
mollyong <1203216292@qq.com>
2021-02-01 23:52:17 +0800

    springboot下载模板

--- src/main/java/com/ramostear/application/controller/FileController.java
+++ src/main/java/com/ramostear/application/controller/FileController.java
@@ -2,8 +2,11 @@ package com.ramostear.application.controller;
 
 import com.ramostear.application.model.FileInfo;
 import com.ramostear.application.util.FileUtil;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -14,10 +17,14 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.PostConstruct;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * @author : ramostear
@@ -38,6 +45,9 @@ public class FileController {
     String fileUploadRootDirLinux;
 
     private static Map<String,FileInfo> fileRepository = new HashMap<>();
+
+//    @Resource
+    private ResourceLoader resourceLoader;
 
     @PostConstruct
     public void initFileRepository(){
@@ -90,24 +100,35 @@ public class FileController {
 
     @GetMapping("/download/{fileName}")
     @ResponseBody
-    public ResponseEntity<Object> downloadFile(@PathVariable(name = "fileName") String fileName) throws FileNotFoundException {
+    public void downloadFile(@PathVariable(name = "fileName") String fileName,
+                             HttpServletResponse response){
+        try {
+            //获取要下载的模板名称
+            //设置要下载的文件的名称
+            response.setHeader("Content-Disposition", "attachment;filename=" + fileName);
+            //通知客服文件的MIME类型
+            response.setContentType("application/vnd.ms-excel;charset=UTF-8");
+            //获取文件的路径
+//            String filePath = getClass().getResource("/"+fileName).getPath();
+            InputStream input = getClass().getClassLoader().getResourceAsStream(fileName);
+//            ByteArrayOutputStream out = new ByteArrayOutputStream();
+//            System.out.println(filePath);
+//            FileInputStream input = new FileInputStream(filePath);
+            OutputStream out = response.getOutputStream();
 
-        File file = new File ( fileUploadRootDir+fileName);
-        InputStreamResource resource = new InputStreamResource ( new FileInputStream ( file ) );
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.add ( "Content-Disposition",String.format("attachment;filename=\"%s",fileName));
-        headers.add ( "Cache-Control","no-cache,no-store,must-revalidate" );
-        headers.add ( "Pragma","no-cache" );
-        headers.add ( "Expires","0" );
-
-        ResponseEntity<Object> responseEntity = ResponseEntity.ok()
-                .headers ( headers )
-                .contentLength ( file.length ())
-                .contentType(MediaType.parseMediaType ( "application/txt" ))
-                .body(resource);
-
-        return responseEntity;
+            byte[] b = new byte[2048];
+            int len;
+            while ((len = input.read(b)) != -1) {
+                out.write(b, 0, len);
+            }
+            out.flush();
+            out.close();
+            input.close();
+        } catch (Exception ex) {
+//            log.error("getApplicationTemplate :", ex);
+            System.out.println(ex);
+            //return Response.ok("应用导入模板下载失败！");
+        }
     }
 
 
